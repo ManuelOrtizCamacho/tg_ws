1c52f0b74683a5c1a32c60c9f5707b448099a889
ManuelOrtizCamacho <Manuel.ortiz@eia.edu.co>
2023-08-09 16:47:25 -0500

    Main

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/manuel/tg_ws/src
+# Build directory: /home/manuel/tg_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("abb_experimental/abb_irb140_gazebo")
+subdirs("abb_experimental/abb_irb140_support")
+subdirs("abb/abb_driver")
---
+++ build/abb_experimental/abb_irb140_support/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/manuel/tg_ws/src/abb_experimental/abb_irb140_support
+# Build directory: /home/manuel/tg_ws/build/abb_experimental/abb_irb140_support
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_abb_irb140_support_roslaunch-check_tests_roslaunch_test.xml "/home/manuel/tg_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/melodic/share/catkin/cmake/test/run_tests.py" "/home/manuel/tg_ws/build/test_results/abb_irb140_support/roslaunch-check_tests_roslaunch_test.xml.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/manuel/tg_ws/build/test_results/abb_irb140_support" "/opt/ros/melodic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/manuel/tg_ws/build/test_results/abb_irb140_support/roslaunch-check_tests_roslaunch_test.xml.xml\" \"/home/manuel/tg_ws/src/abb_experimental/abb_irb140_support/tests/roslaunch_test.xml\" ")
